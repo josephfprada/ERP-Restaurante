52645005809f6cfea67babeefb41dc69c1149e6b
josephfprada <josephprada.ar@gmail.com>
2025-03-28 13:53:38 -0500

    Add files via upload

---
+++ Modulos/Trabajadores/Code/Core/administrador.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import (
+    Empleado, Departamento, EmpleadoDepartamento, 
+    Capacitacion, Beneficios, Desempeno, Permiso, RelacionLaboral
+)
+
+admin.site.register(Empleado)
+admin.site.register(Departamento)
+admin.site.register(EmpleadoDepartamento)
+admin.site.register(Capacitacion)
+admin.site.register(Beneficios)
+admin.site.register(Desempeno)
+admin.site.register(Permiso)
+admin.site.register(RelacionLaboral)
---
+++ Modulos/Trabajadores/Code/Django/Settings/Dependencias.py
@@ -0,0 +1,12 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Your HR Module
+    'HumanResources',  # <-- Make sure your app is listed here
+    'rest_framework',   # If using Django REST Framework
+]
