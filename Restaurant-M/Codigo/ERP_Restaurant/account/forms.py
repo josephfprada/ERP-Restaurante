377cd8442a7901dbcefbfea0d1de8f0231b9b045
Wilder14lis <111709899+Wilder14lis@users.noreply.github.com>
2025-04-27 13:14:36 -0500

    Delete Restaurant-M/Codigo/ERP_Restaurant directory

--- Restaurant-M/Codigo/ERP_Restaurant/account/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-# Formulario de loggeo
-class LoginForm(forms.Form):
-    email = forms.EmailField(label='Correo electrónico', max_length=100)
-    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
